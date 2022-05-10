b0ab70eff33fb40e2269641898e9760e383656bd
Akbar Rahmat Mulyatama <akbarrahmatmulyatama@gmail.com>
2022-05-10 17:50:58 +0700

    import bufferedreader

--- Obat.java
+++ Obat.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Obat {
     public static void main(String[] args) {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+
+        int menu;
+
         System.out.println("========================================");
         System.out.println("\t\tMenu Data");
         System.out.println("========================================");
@@ -7,6 +14,6 @@ public class Obat {
         System.out.println("2. Cetak data obat");
         System.out.println("3. Keluar");
         System.out.println("========================================");
-
+        System.out.println("Masukkan pilihan anda [1/2/3] ? ");
     }
 }
