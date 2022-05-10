b83b8d91e439c5fcaab52af262fc8bcc42b0f828
Akbar Rahmat Mulyatama <akbarrahmatmulyatama@gmail.com>
2022-05-10 17:44:07 +0700

    First commit

---
+++ Obat.java
@@ -0,0 +1,12 @@
+public class Obat {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("\t\tMenu Data");
+        System.out.println("========================================");
+        System.out.println("1. Input data obat");
+        System.out.println("2. Cetak data obat");
+        System.out.println("3. Keluar");
+        System.out.println("========================================");
+
+    }
+}
